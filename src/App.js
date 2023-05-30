6c8cab22ee7d90293259e11773ac6a5f320c81eb
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-05-30 20:13:04 +0500

    login redux isLoading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Main, Login, Register } from "./components/";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-    
+      <Navbar />
       <Routes>
         <Route path="/" exect element={<Main />} />
         <Route path="/login" element={<Login />} />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
-import {Input} from "../Ui/index";
+import { Input } from "../Ui/index";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUserStart } from "../slice/auth.js";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.auth);
+
+  const loginHandler = (e) => {
+    dispatch(loginUserStart());
+    e.preventDefault();
+  };
 
   return (
     <div className="text-center mt-5">
@@ -18,13 +27,22 @@ function Login() {
           />
           <h1 className="h3 mb-3 fw-normal">Please Login</h1>
 
-          <Input label="Email address" state={email} setState={setEmail}/>
-          <Input label="Password" type={password} state={password} setState={setPassword}/>
+          <Input label="Email address" state={email} setState={setEmail} />
+          <Input
+            label="Password"
+            type={password}
+            state={password}
+            setState={setPassword}
+          />
 
-          <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">
-            Login
+          <button
+            className="w-100 btn btn-lg btn-primary mt-2"
+            disabled={isLoading}
+            onClick={loginHandler}
+            type="submit"
+          >
+            {isLoading ? "Loading" : "Login"}
           </button>
-
         </form>
       </main>
     </div>
@@ -32,4 +50,3 @@ function Login() {
 }
 
 export default Login;
-
