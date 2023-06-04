5f734f7b8dc08f8b958eefe12531ccf2c271d54a
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-06-04 18:45:35 +0500

    localstorage & token

--- src/slice/auth.js
+++ src/slice/auth.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import setItem from "../helpers/persistance-storage";
 
 const initialState = {
   isLoading: false,
@@ -18,7 +19,7 @@ export const authSlice = createSlice({
       state.loggedIn = true;
       state.isLoading = false;
       state.user = action.payload;
-      console.log(state.user);
+      setItem("token", action.payload.token);
     },
     signUserFailure: (state, action) => {
       state.isLoading = false;
