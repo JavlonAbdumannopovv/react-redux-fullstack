0d2d4d21f985029fd34cedc43f8aae9d83723b51
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-06-07 18:12:34 +0500

    Loader

---
+++ src/ui/loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className="d-block mx-auto spinner-border">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  )
+}
+
+export default Loader;
