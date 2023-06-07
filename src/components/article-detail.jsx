9ba401eafecb34e6e99d82a4cba4b41d2328aac1
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-06-07 19:57:52 +0500

    create article ui

--- src/components/article-detail.jsx
+++ src/components/article-detail.jsx
@@ -1,32 +1,51 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import ArticleService from "../service/article";
-import { useDispatch } from 'react-redux';
-import { getArticleDetailStart, getArticleDetailSuccess } from '../slice/article';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getArticleDetailStart,
+  getArticleDetailSuccess,
+} from "../slice/article";
+import moment from "moment";
+import { Loader } from "../ui/index";
 
 function ArticleDetail() {
-  const {slug} = useParams();
-  const dispatch = useDispatch(state => state.article);
+  const { slug } = useParams();
+  const dispatch = useDispatch();
+  const { articleDetail, isLoading } = useSelector((state) => state.article);
 
-  const getArticleDetail = async() => {
+  const getArticleDetail = async () => {
     dispatch(getArticleDetailStart());
-    try{
+    try {
       const response = await ArticleService.getArticleDetail(slug);
       dispatch(getArticleDetailSuccess(response));
-    }catch (error){
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getArticleDetail();
-  }, [slug])
+  }, [slug]);
 
-  return (
-    <div>
-      slug: {slug}
-    </div>
-  )
+  return isLoading ? (
+    <Loader />
+  ) : (
+    articleDetail !== null && (
+      <div className="p-5 mb-4 rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">{articleDetail.title}</h1>
+          <p className="col-md-8 fs-4">{articleDetail.description}</p>
+        </div>
+        <p className="text-muted">
+          <span className="fw-bold">
+            Created at: {moment(articleDetail.createdAt).format("DD MMM, YYYY")}
+          </span>
+        </p>
+        <div>{articleDetail.body}</div>
+      </div>
+    )
+  );
 }
 
-export default ArticleDetail
+export default ArticleDetail;
